483426ddf7ed500bb333a90d893815199b82eeb7
Pond_Chaitana <dexteriez.13052@gmail.com>
2025-03-16 18:54:51 +0700

    fix

--- app.py
+++ app.py
@@ -168,8 +168,8 @@ with tab4:
     st.image("photo/Graph.png")
 
     # โหลดโมเดล
-    model = tf.keras.models.load_model('model/mobilenetv2_cat_dog.h5')
-    model.save('model/mobilenetv2_cat_dog')
+    model = tf.keras.models.load_model('model/mobilenetv2_cat_dog.h5', compile=False)
+    model.save('model/mobilenetv2_cat_dog_saved', save_format='tf')
 
     # ฟังก์ชันทำนายภาพ
     def predict_image(image):
